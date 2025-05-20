1f4fb342d482fddb52cecfd45bebf53f2eb249d7
gocha7777 <realme.masterbin@gmail.com>
2025-05-21 03:19:21 +0500

    Resolve merge conflict in README.md

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/pautinka/', // Указываем префикс для деплоя на GitHub Pages
   build: {
     rollupOptions: {
       external: ['framer-motion'], // Явно указываем зависимость как внешнюю
     },
   },
   scss: {
-    // Additional options if needed
-    additionalData: `@import "./cssPages/Posts.scss";`, // Example: Import global SCSS variables
-  }, server: {
+    additionalData: `@import "./cssPages/Posts.scss";`, // Пример импорта глобальных переменных SCSS
+  },
+  server: {
     allowedHosts: [
       'localhost', // Разрешаем localhost
       'bcdb-77-238-245-33.ngrok-free.app', // Добавляем домен ngrok
