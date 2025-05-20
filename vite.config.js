edbfac393763bb5a5a930511da2719605d5dca95
gocha7777 <realme.masterbin@gmail.com>
2025-05-21 03:24:34 +0500

    Resolve merge conflict in README.md

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pautinka/',
+  base: '/Pautinka/',
   build: {
     rollupOptions: {
       external: ['framer-motion'], // Явно указываем зависимость как внешнюю
